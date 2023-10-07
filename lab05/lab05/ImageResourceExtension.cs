d9117e353e70e440bfda987acaa0f045e36f28d2
Xavier Mamani <xavier.mamani@tecsup.edu.pe>
2023-10-07 16:46:26 -0500

    Agregar archivos de proyecto.

---
+++ lab05/lab05/ImageResourceExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+
+namespace lab05
+{
+    [Preserve(AllMembers = true)]
+    [ContentProperty(nameof(Source))]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+                return null;
+
+            //Do your translation lookup here, using whatever method you 
+            var imageSource = ImageSource.FromResource(Source,
+                typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+            return imageSource;
+        }
+    }
+}
